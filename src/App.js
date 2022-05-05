3d9f7cd9ad1923d20a477317757cc52abeb2e504
sejalmishra <sejalmishra1310@gmail.com>
2022-05-05 21:24:50 +0530

    ghpages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Spash from './Spash';
 import Login from './Login';
 import Home from './Home';
 import Settings from './Settings';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router,HashRouter, Route, Routes } from 'react-router-dom';
 import Onboard from './Onboard';
 import Check from './Check';
 import Exam from './Exam';
@@ -11,7 +11,7 @@ import Exam from './Exam';
 function App() {
   return (
     <Router>
-      <Routes>
+      <HashRouter>
         <Route exact path='/' element={<Spash/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/onboard' element={<Onboard/>}/>
@@ -19,7 +19,7 @@ function App() {
         <Route path='/settings' element={<Settings/>}/>
         <Route path='/check' element={<Check/>}/>
         <Route path='/exam' element={<Exam/>}/>
-      </Routes>
+      </HashRouter>
     </Router>
   );
 }
